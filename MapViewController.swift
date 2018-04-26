714c5f8cbae84661dcbc5a7c22f10f6aeb6ec90b
zxyBlueCat <1037684652@qq.com>
2018-04-26 17:50:45 +0800

    2018-4-26-2

---
+++ MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by hadoop on 2018/4/26.
+//  Copyright © 2018年 hadoop. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("map加载成功")
+    }
+}
